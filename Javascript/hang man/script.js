1f13d0e1b18f3de02096e8df27211be21e15791b
aduarte95 <aduarte.roldan@gmail.com>
2020-01-22 16:19:25 -0600

    hangman

--- Javascript/hang man/script.js
+++ Javascript/hang man/script.js
@@ -1,10 +1,17 @@
 var words = ['hola', 'caballo', 'jaja']
 var selectedWord;
-var state;
+var state = {
+    cont: 0,
+    length: 0
+};
 
 function init() {
     var wordIndex = Math.floor(Math.random() * words.length)
-    selectedWord = words[wordIndex]
+    selectedWord = words[wordIndex];
+    state.length = selectedWord.length;
+    [...selectedWord].forEach( letter => { state[letter] = false; } )
+
+
     var word = document.querySelector('.word');
     var abc = document.querySelector('.abc');
     var node;
@@ -18,7 +25,7 @@ function init() {
 
     for(let i = 0; i < 26; ++i) {
         node = document.createElement('button');
-        node.innerHTML = (i+10).toString(36);
+        node.innerHTML = String.fromCharCode(i+97);
         node.className = 'abc-letter'
         node.id = 'abc-letter' + i
         node.addEventListener('click', verifyLetter);
@@ -26,17 +33,36 @@ function init() {
     }
 }
 
+function win(){
+    for(let i = 0; i < 26; ++i) {
+        buttonLetter = document.getElementById('abc-letter'+ i);
+        buttonLetter.disabled = true;
+    }
+}
+
+
 function verifyLetter() {
     var abcLetter = this.innerHTML;
     var fillLetter;
+    var buttonLetter;
+    var asciiCode;
 
     [...selectedWord].forEach(
         function(letter, i) {
-            console.log(letter)
             if(letter === abcLetter) {
+                state[letter] = true;
+                state.cont++
                 fillLetter = document.getElementById('letter'+i);
+                buttonLetter = document.getElementById('abc-letter'+ (abcLetter.charCodeAt(0)-97).toString());
                 fillLetter.innerHTML = abcLetter;
+                buttonLetter.disabled = true;
+            } else {
+                
             }
         }
     )
+
+    if(state.cont == state.length) {
+    win();
+    }
 }
