c1d9b5f7a3ecc2c326997e8d23269616dd0d998c
aduarte95 <aduarte.roldan@gmail.com>
2020-02-07 17:07:18 -0600

    Hide

--- project-two/src/components/QuizForm/QuizForm.jsx
+++ project-two/src/components/QuizForm/QuizForm.jsx
@@ -5,13 +5,22 @@ import ssQuizData from '../../data/ssQuiz.json'
 function QuizForm() {
   const [ssQuiz, setSsQuiz] = useState([]);
   const [numQuestion, setNumQuestions] = useState(0);
-  const [selectedAnwers, setSelectedAnswers] = useState([]);
-  const [showScore, setShowScore] = useState(0);
+  const [selectedAnwers, setSelectedAnswers] = useState({});
+  const [showScore, setShowScore] = useState(false);
 
   function getScore(e) {
     e.preventDefault();
-    console.log(selectedAnwers)
-    
+    var score = 0;
+
+    if(ssQuiz) {
+      ssQuiz.map( (q, i) => {
+        if(q.correctAnswer === Number(selectedAnwers[i])) {
+          score++
+        }
+      })
+    }
+
+    setShowScore(true);
   }
 
   function setAnswers(e) {
@@ -21,8 +30,7 @@ function QuizForm() {
     const id = e.target.id;
     var answer = id.split("-");
     
-    setSelectedAnswers( oldArray => [{[question[1]]: answer[1], ...oldArray[0]}])
-    
+    setSelectedAnswers( oldObject => ({...oldObject[0], [question[1]]: answer[1]}))
   }
   
   useEffect(() => {
@@ -34,31 +42,38 @@ function QuizForm() {
   return (
     <div className="container">
       <div className="d-flex justify-content-center align-items-center">
-        <img className="quiz-container__decorator-img" src="
-            https://spaceplace.nasa.gov/review/planets/planets-13.sp.png" alt="Friendly Jupiter"></img>
-        <form className="quiz-container quiz-container--margin-bottom glowing-box">
-          { ssQuiz.map( (question, i) => {
-            return    <div key={`question-${i}`} className="form-check quiz-container--margin-bottom">
-                        <label className="quiz-container__label quiz-container--margin-bottom"> {question.label} </label>
-                        { 
-                          question.answers.map( (a,j) => {   
+        { !showScore &&
+         <div>
+            <img className="quiz-container__decorator-img" src="
+              https://spaceplace.nasa.gov/review/planets/planets-13.sp.png" alt="Friendly Jupiter"></img>
+            <form className="quiz-container quiz-container--margin-bottom glowing-box">
+              { ssQuiz.map( (question, i) => {
+                return    <div key={`question-${i}`} className="form-check quiz-container--margin-bottom">
+                            <label className="quiz-container__label quiz-container--margin-bottom"> {question.label} </label>
+                            { 
+                              question.answers.map( (a,j) => {   
 
-                            return  <div key={`answer-${j}`} className="quiz-container--margin-bottom-1"> 
-                                      <div className="custom-control custom-radio">
-                                          <input type="radio" onChange={(e) => setAnswers(e)} id={`customRadio-${a.id}`} name={`customRadio-${i}`} className="custom-control-input"/>
-                                          <label className="custom-control-label" htmlFor={`customRadio-${a.id}`}>{a.answer}</label>
+                                return  <div key={`answer-${j}`} className="quiz-container--margin-bottom-1"> 
+                                          <div className="custom-control custom-radio">
+                                              <input type="radio" onChange={(e) => setAnswers(e)} id={`customRadio-${a.id}`} name={`customRadio-${i}`} className="custom-control-input"/>
+                                              <label className="custom-control-label" htmlFor={`customRadio-${a.id}`}>{a.answer}</label>
+                                            </div>
                                         </div>
-                                    </div>
-                          })
-                          
-                        }
-                      </div>
-          })
-          }
-          <div className="d-flex justify-content-center quiz-container__submit-btn">
-            <button type="submit" className="btn btn-primary" onClick={(e) => getScore(e)}>Submit</button>
-          </div>
-        </form> 
+                              })
+                              
+                            }
+                          </div>})
+              }
+              <div className="d-flex justify-content-center quiz-container__submit-btn">
+                <button type="submit" className="btn btn-primary" onClick={(e) => getScore(e)}>Submit</button>
+              </div>
+            </form> 
+        </div> }
+        { showScore &&
+        <div className="score-container">Hola
+        </div>
+
+        }
       </div>
     </div>
   );
