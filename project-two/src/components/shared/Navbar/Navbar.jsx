770acc1b58a70340c6a44f16d5f2e1a209026167
aduarte95 <aduarte.roldan@gmail.com>
2020-02-09 22:48:58 -0600

    Navbar links

--- project-two/src/components/shared/Navbar/Navbar.jsx
+++ project-two/src/components/shared/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Navbar.scss';
-import {Link} from 'react-router-dom';
+import {NavLink, Link} from 'react-router-dom';
 
 function Navbar() {
   
   return (
     <nav className="navbar-container navbar navbar-expand-lg navbar-dark primary-background">
-      <Link to="/" className="navbar-brand d-flex align-items-center">
+      <div className="navbar-brand d-flex align-items-center">
         <img src="https://static.thenounproject.com/png/40868-84.png" width="50" height="50" className="navbar-container__logo-img d-inline-block align-top" alt=""/>
         <div className="navbar-container__logo-text">
           <div className="navbar-container__logo-text-1">
@@ -16,7 +16,7 @@ function Navbar() {
             Conquerors
           </div>
         </div>
-      </Link>
+      </div>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -24,25 +24,25 @@ function Navbar() {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
 
-      <li className="nav-item active">
-        <Link to="/" className="nav-link"> <span className="discover">Home</span> <span className="sr-only">(current)</span></Link>
+      <li className="nav-item">
+        <NavLink activeClassName="selected-link"  exact to="/" className="nav-link"> Home </NavLink>
       </li>
       
       <li className="nav-item">
-        <Link to="/gallery" className="nav-link"> Gallery </Link>
+        <NavLink activeClassName="selected-link"  to="/gallery" className="nav-link"> Gallery </NavLink>
       </li>
 
       <li className="nav-item">
-        <Link to="/cool-sites" className="nav-link"> Cool Sites </Link>
+        <NavLink activeClassName="selected-link"  activeClassName="selected-link" to="/cool-sites" className="nav-link"> Cool Sites </NavLink>
       </li>
 
       <li className="nav-item">
-        <Link to="/" className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <Link activeClassName="selected-link"  activeClassName="selected-link"  exact to="/" className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Learn About
         </Link>
         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
           <Link to="/" className="dropdown-item" href="/">Animals</Link>
-          <Link to="/" className="dropdown-item" href="/">Solar System</Link>
+          <Link activeClassName="selected-link"  to="/" className="dropdown-item" href="/">Solar System</Link>
         </div>
       </li>
     </ul>
