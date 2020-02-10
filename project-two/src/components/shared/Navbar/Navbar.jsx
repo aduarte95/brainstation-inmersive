62fccd0055c57b320009f57d8f0cbe99b09f1dae
aduarte95 <aduarte.roldan@gmail.com>
2020-02-09 22:49:08 -0600

    Nav-bar

--- project-two/src/components/shared/Navbar/Navbar.jsx
+++ project-two/src/components/shared/Navbar/Navbar.jsx
@@ -17,43 +17,44 @@ function Navbar() {
           </div>
         </div>
       </div>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-      <li className="nav-item">
-        <NavLink activeClassName="selected-link"  exact to="/" className="nav-link"> Home </NavLink>
-      </li>
-      
-      <li className="nav-item">
-        <NavLink activeClassName="selected-link"  to="/gallery" className="nav-link"> Gallery </NavLink>
-      </li>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
 
-      <li className="nav-item">
-        <NavLink activeClassName="selected-link"  activeClassName="selected-link" to="/cool-sites" className="nav-link"> Cool Sites </NavLink>
-      </li>
+          <li className="nav-item">
+            <NavLink activeClassName="selected-link"  exact to="/" className="nav-link"> Home </NavLink>
+          </li>
+          
+          <li className="nav-item">
+            <NavLink activeClassName="selected-link"  to="/gallery" className="nav-link"> Gallery </NavLink>
+          </li>
 
-      <li className="nav-item">
-        <Link activeClassName="selected-link"  activeClassName="selected-link"  exact to="/" className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Learn About
-        </Link>
-        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <Link to="/" className="dropdown-item" href="/">Animals</Link>
-          <Link activeClassName="selected-link"  to="/" className="dropdown-item" href="/">Solar System</Link>
-        </div>
-      </li>
-    </ul>
+          <li className="nav-item">
+            <NavLink activeClassName="selected-link" to="/cool-sites" className="nav-link"> Cool Sites </NavLink>
+          </li>
 
-    <form className="form-inline my-2 my-lg-0">
-      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+          <li className="nav-item">
+            <Link exact to="/" className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Learn About
+            </Link>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <Link to="/" className="dropdown-item" href="/">Animals</Link>
+              <Link to="/" className="dropdown-item" href="/">Solar System</Link>
+            </div>
+          </li>
+        </ul>
 
-  </div>
-</nav>
+        <form className="form-inline my-2 my-lg-0">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+
+      </div>
+    </nav>
   );
 }
 
