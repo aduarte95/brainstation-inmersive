3ba8e24a0d96cb59f630c0fd40c58274749883b9
aduarte95 <aduarte.roldan@gmail.com>
2020-02-10 10:39:11 -0600

    New library for transitions

--- project-two/src/components/shared/InfoChart/InfoChart.jsx
+++ project-two/src/components/shared/InfoChart/InfoChart.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import './InfoChart.scss';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; // ES6
+import { CSSTransition } from 'react-transition-group';
+
 
 function InfoChart({children, className}) {
   
   return (
-    <ReactCSSTransitionGroup
-      transitionName="info-chart"
-      transitionAppear={true}
-      transitionAppearTimeout={10000}
-      transitionEnter={false}
-      transitionLeave={false}>
+    <CSSTransition in={true}
+    timeout={10000}
+    classNames="info-chart"
+    unmountOnExit
+    appear>
       <div className={"info-chart-container glowing-box " + className}>
           <p className="info-chart-container__text">
               {children}
           </p>
       </div>
-    </ReactCSSTransitionGroup>
+    </CSSTransition>
   );
 }
 
